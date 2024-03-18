9a6e4dc366a6518df0cb1fabf1b777e8b58676e6
vonserfidelity <163811964+vonserfidelity@users.noreply.github.com>
2024-03-18 10:53:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/bfhl', (req, res) => {
+  const data = req.body.data;
+  const userId = `vaibhav`;
+  const email = `vaibhav1862.be21@chitkara.edu.in`;
+  const rollNumber = `2110991862`;
+
+  const oddNumbers = [];
+  const evenNumbers = [];
+  const alphabets = [];
+
+  data.forEach(item => {
+    if (typeof item === 'number') {
+      if (item % 2 === 0) {
+        evenNumbers.push(item);
+      } else {
+        oddNumbers.push(item);
+      }
+    } else if (typeof item === 'string') {
+      if (/^[A-Za-z]+$/.test(item)) {
+        alphabets.push(item.toUpperCase());
+      }
+    }
+  });
+
+  const response = {
+    is_success: true,
+    user_id: userId,
+    email: email,
+    roll_number: rollNumber,
+    odd_numbers: oddNumbers,
+    even_numbers: evenNumbers,
+    alphabets: alphabets
+  };
+
+  res.json(response);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
