9c97478f05e07d2c060fb4422fb038160859d0fd
vonserfidelity <163811964+vonserfidelity@users.noreply.github.com>
2024-03-18 12:00:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json()); // Add this line to parse JSON request bodies
+app.use(bodyParser.json()); 
 
 app.post('/bfhl', (req, res) => {
   const data = req.body;
